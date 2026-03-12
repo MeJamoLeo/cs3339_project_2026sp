38b1f7e061d03f16f62bc724715e5c965cce1e7c
Reo Tajiri <kif33@txstate.edu>
2026-03-12 02:40:07 -0500

    log: TODO

--- main.lisp
+++ main.lisp
@@ -3,6 +3,7 @@
   (+ a b))
 
 ;; Read input file
+;; TODO: devide this in two functions; one is read assembly, the other is instruction encoder
 (defun assembler ()
   (with-open-file (input-stream "input")
 	(loop for line = (read-line input-stream nil)
