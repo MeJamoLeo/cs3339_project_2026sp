7b9c5222c8ef20673d75d229fc99ba2fd72554c9
Reo Tajiri <kif33@txstate.edu>
2026-03-12 01:44:21 -0500

    log: read input file content
    refac: use with-open-file as default for readability

--- main.lisp
+++ main.lisp
@@ -1,3 +1,10 @@
 ;; adder
 (defun adder (a b)
   (+ a b))
+
+;; Read input file
+(defun assembler ()
+  (with-open-file (input-stream "input")
+	(loop for line = (read-line input-stream nil)
+		  while line
+		  do (format t "~A~%" line))))
