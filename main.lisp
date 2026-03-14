096e8f232a04569415b7f3384dfb06c4dc88ac54
Reo Tajiri <kif33@txstate.edu>
2026-03-14 03:59:52 -0500

    log: remove comment

--- main.lisp
+++ main.lisp
@@ -1,9 +1,6 @@
-;; adder
 (defun adder (a b)
   (+ a b))
 
-;; Read input file
-;; TODO: devide this in two functions; one is read assembly, the other is instruction encoder
 (defun read-assembly (path)
   (with-open-file (input-stream path)
 	(loop for line = (read-line input-stream nil)
