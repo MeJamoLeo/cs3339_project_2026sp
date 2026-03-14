7002e5962f18b3471865e0b0751d14f5e68e6a9c
Reo Tajiri <kif33@txstate.edu>
2026-03-14 01:06:59 -0500

    log: fix split-by-one for multiple spaces

--- main.lisp
+++ main.lisp
@@ -10,10 +10,12 @@
 		  while line
 		  collect line)))
 
-(defun split-by-one-space (line)
+(defun split-by-spaces (line)
     (loop for i = 0 then (1+ j)
           as j = (position #\Space line :start i)
-          collect (subseq line i j)
+          as token = (subseq line i j)
+		  when (string/= token "")
+		  collect token
           while j))
 
-(pprint(split-by-one-space "add $s0, $t0, $t1"))
+(pprint(split-by-one-space "add   $s0 $t0 $t1"))
