bfdbdb979a57568ee2f4432ee5ea5072560de2ec
Reo Tajiri <kif33@txstate.edu>
2026-03-16 22:39:09 -0500

    add: excode-instruction
    Sample Output
    (32 34 8 35)

--- main.lisp
+++ main.lisp
@@ -20,8 +20,6 @@
 (defun parse-assembly (path)
   (mapcar #'split-by-spaces (read-assembly path)))
 
-(defun encode-instruction (inst)
-  (gethash inst *instruction-memory-table*))
 (defparameter *register-table* (let ((ht (make-hash-table :test #'equal)))
 									 (loop for (name num) in
 										   '(("$zero" 0) ("$at" 1) ("$v0" 2) ("$v1" 3) ("$a0" 4) ("$a1" 5) ("$a2" 6) ("$a3" 7) ("$t0" 8) ("$t1" 9) ("$t2" 10) ("$t3" 11) ("$t4" 12) ("$t5" 13) ("$t6" 14) ("$t7" 15) ("$s0" 16) ("$s1" 17) ("$s2" 18) ("$s3" 19) ("$s4" 20) ("$s5" 21) ("$s6" 22) ("$s7" 23) ("$t8" 24) ("$t9" 25) ("$k0" 26) ("$k1" 27) ("$gp" 28) ("$sp" 29) ("$fp" 30) ("$ra" 31))
@@ -45,11 +43,9 @@
 												   ("nop" :r #b000000)) ;; 0, branch if equal to
 												 do (setf (gethash name ht) (list inst-type num)))
 										   ht))
-(pprint *instruction-memory-table*)
-(format t "Hash Table Count: ~A~%" (hash-table-count *instruction-memory-table*))
-(format t "Instruction Memory Hash: ~B~%" (gethash "add" *instruction-memory-table*))
-(format t "Instruction Memory Hash: ~A~%" (gethash "add" *instruction-memory-table*))
-(format t "Instruction Memory Hash: ~B~%" (gethash "lw" *instruction-memory-table*))
-(format t "Instruction Memory Hash: ~A~%" (gethash "lw" *instruction-memory-table*))
-(pprint (encode-instruction "add"))
+
+(defun encode-instruction (instructions)
+  (mapcar (lambda (x) (car (cdr (gethash x *instruction-memory-table*)))) instructions))
+
+(pprint (encode-instruction '("add" "sub" "addi" "lw")))
 
