c267a33346465f586ef13a959294edf665f2234f
Reo Tajiri <kif33@txstate.edu>
2026-04-05 16:21:47 -0500

    log: add unit test split-by-spaces
    This unit test shows me how the function works.
    i don't have to memorize input and output for function.

--- test.lisp
+++ test.lisp
@@ -9,9 +9,21 @@
 (assert (equal (normalize "lw $t2, 0($sp)") "lw $t2 0 $sp "))
 (assert (equal (normalize "nop") "nop"))
 
+;; ------------------------------------ split-by-spaces
+(assert (equal (split-by-spaces "I have a pen.")
+			   '("I" "have" "a" "pen.")))
+(assert (equal (split-by-spaces "addi $t0 $zero 5")
+			   '("addi" "$t0" "$zero" "5")))
+
 ;; ====================================
 ;; CPU
 ;; ====================================
 ;; ------------------------------------ adder
 (assert (= (adder 0 4) 4))
 
+
+
+
+
+
+(format t "✅ All test passed!!")
