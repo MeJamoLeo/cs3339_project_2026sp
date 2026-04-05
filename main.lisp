233cbd0f4102d6207616e2731e1d8eb43bbc7370
Reo Tajiri <kif33@txstate.edu>
2026-04-05 14:06:55 -0500

    format

--- main.lisp
+++ main.lisp
@@ -1,33 +1,33 @@
 (defparameter *register-table* (let ((ht (make-hash-table :test #'equal)))
-									 (loop for (name num) in
-										   '(("$zero" 0) ("$at" 1) ("$v0" 2) ("$v1" 3)
-											 ("$a0" 4) ("$a1" 5) ("$a2" 6) ("$a3" 7)
-											 ("$t0" 8) ("$t1" 9) ("$t2" 10) ("$t3" 11)
-											 ("$t4" 12) ("$t5" 13) ("$t6" 14) ("$t7" 15)
-											 ("$s0" 16) ("$s1" 17) ("$s2" 18) ("$s3" 19)
-											 ("$s4" 20) ("$s5" 21) ("$s6" 22) ("$s7" 23)
-											 ("$t8" 24) ("$t9" 25) ("$k0" 26) ("$k1" 27)
-											 ("$gp" 28) ("$sp" 29) ("$fp" 30) ("$ra" 31))
-										   do (setf (gethash name ht) num))
-									 ht))
+								 (loop for (name num) in
+									   '(("$zero" 0) ("$at" 1) ("$v0" 2) ("$v1" 3)
+													 ("$a0" 4) ("$a1" 5) ("$a2" 6) ("$a3" 7)
+													 ("$t0" 8) ("$t1" 9) ("$t2" 10) ("$t3" 11)
+													 ("$t4" 12) ("$t5" 13) ("$t6" 14) ("$t7" 15)
+													 ("$s0" 16) ("$s1" 17) ("$s2" 18) ("$s3" 19)
+													 ("$s4" 20) ("$s5" 21) ("$s6" 22) ("$s7" 23)
+													 ("$t8" 24) ("$t9" 25) ("$k0" 26) ("$k1" 27)
+													 ("$gp" 28) ("$sp" 29) ("$fp" 30) ("$ra" 31))
+									   do (setf (gethash name ht) num))
+								 ht))
 
 (defparameter *instruction-table* (let ((ht (make-hash-table :test #'equal)))
-										   (loop for (name inst-type opcode-or-funct order) in
-												 '(("add" :r #b100000 (rd rs rt)) ;;32, signed integer addition
-												   ("addi" :i #b001000 (rt rs imm)) ;; 8, add immediate
-												   ("sub" :r #b100010 (rd rs rt)) ;; 34, signed integer subtraction
-												   ;("mult" :r #b011000 ()) ;; 24, integer multiplication
-												   ("and" :r #b100100 (rd rs rt)) ;; 36, bitwise and operation
-												   ("or" :r #b100101 (rd rs rt)) ;; 37, bitwise or operation
-												   ("sll" :r #b000000 (rd rt shamt)) ;; 0, shift left logical
-												   ("srl" :r #b000010 (rd rt shamt)) ;; 2, shift right logical
-												   ("lw" :i #b100011 (rt imm rs)) ;; 35, load word
-												   ("sw" :i #b101011 (rt imm rs)) ;; 43, store word
-												   ("beq" :i #b000100 (rs rt imm)) ;; 4, branch if equal to
-												   ("j" :j #b000010 (addr)) ;; 2, jump
-												   ("nop" :r #b000000)) ;; 0, no operation
-												 do (setf (gethash name ht) (list inst-type opcode-or-funct order)))
-										   ht))
+									(loop for (name inst-type opcode-or-funct order) in
+										  '(("add" :r #b100000 (rd rs rt)) ;;32, signed integer addition
+											("addi" :i #b001000 (rt rs imm)) ;; 8, add immediate
+											("sub" :r #b100010 (rd rs rt)) ;; 34, signed integer subtraction
+											;("mult" :r #b011000 ()) ;; 24, integer multiplication
+											("and" :r #b100100 (rd rs rt)) ;; 36, bitwise and operation
+											("or" :r #b100101 (rd rs rt)) ;; 37, bitwise or operation
+											("sll" :r #b000000 (rd rt shamt)) ;; 0, shift left logical
+											("srl" :r #b000010 (rd rt shamt)) ;; 2, shift right logical
+											("lw" :i #b100011 (rt imm rs)) ;; 35, load word
+											("sw" :i #b101011 (rt imm rs)) ;; 43, store word
+											("beq" :i #b000100 (rs rt imm)) ;; 4, branch if equal to
+											("j" :j #b000010 (addr)) ;; 2, jump
+											("nop" :r #b000000)) ;; 0, no operation
+										  do (setf (gethash name ht) (list inst-type opcode-or-funct order)))
+									ht))
 
 (defun adder (a b)
   (+ a b))
@@ -101,10 +101,10 @@
 	(format t "addr\(~A\)~C~26,'0B~%"  inst25-0 #\Tab inst25-0)))
 
 (mapcar (lambda (line)
-			(format t "~A~%" "=======================================")
-			(format t "~A~%" line)
-			(format t "~A~%" "---------------------------------------")
-			(decode (encode line))
-			(format t "~A~%~%" "======================================="))
+		  (format t "~A~%" "=======================================")
+		  (format t "~A~%" line)
+		  (format t "~A~%" "---------------------------------------")
+		  (decode (encode line))
+		  (format t "~A~%~%" "======================================="))
 		(parse-assembly "./input"))
 
